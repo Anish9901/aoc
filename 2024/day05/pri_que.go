307b1bff420daefd62e063c8995a76135623960e
Anish Umale <umaleanish120@gmail.com>
2024-12-09 01:52:59 +0530

    solution for Day 05: Print Queue(part 2)

--- 2024/day05/pri_que.go
+++ 2024/day05/pri_que.go
@@ -18,10 +18,24 @@ func check_rule(ordering_rule []string, print_input []string) int {
 	valid_middle_pg := 0
 	for i := range len(print_input) - 1 {
 		for j := i + 1; j < len(print_input); j++ {
-			//fmt.Println(print_input[j] + "|" + print_input[i])
 			if slices.Contains(ordering_rule, print_input[j] + "|" + print_input[i]) {
-				//fmt.Println("contains")
 				return 0
+			}
+		}
+	}
+	valid_middle_pg, _ = strconv.Atoi(print_input[(len(print_input) + 1)/2 - 1])
+	return valid_middle_pg
+}
+
+func correct_ordering(ordering_rule []string, print_input []string) int {
+	valid_middle_pg := 0
+	for i := range len(print_input) - 1 {
+		for j := i + 1; j < len(print_input); j++ {
+			if slices.Contains(ordering_rule, print_input[j] + "|" + print_input[i]) {
+				swap := print_input[i]
+				print_input[i] = print_input[j]
+				print_input[j] = swap
+				j = i + 1
 			}
 		}
 	}
@@ -35,14 +49,16 @@ func Run() {
 	raw_input := s.Split(string(dat), "\n\n")
 	ordering_rule := s.Split(raw_input[0], "\n")
 	given_ordering := s.Split(raw_input[1], "\n")
-	// fmt.Println(ordering_rule)
-	// fmt.Println(given_ordering)
 	middle_pg_no := 0
+	order_corrected_middle_pg_no_sum := 0
 	for i := range len(given_ordering) {
 		print_input := s.Split(given_ordering[i], ",")
 		// fmt.Println(print_input)
 		middle_pg_no += check_rule(ordering_rule, print_input)
-		// fmt.Println(check_rule(ordering_rule, print_input))
+		if check_rule(ordering_rule, print_input) == 0 {
+			order_corrected_middle_pg_no_sum += correct_ordering(ordering_rule, print_input)
+		}
 	}
 	fmt.Println(middle_pg_no)
+	fmt.Println(order_corrected_middle_pg_no_sum)
 }
